9e9d1558e36e9d91b7cd1200f98529e026ab9ec3
Jack Nystrom <nystromjp@gmail.com>
2023-06-07 00:40:11 -0700

    fix exts

---
+++ redeploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Clone website repo if first time, else pull
+cd /root
+if ! [ -d "/root/bvngeecord.com/.git" ]; then
+    git clone https://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@github.com/BvngeeCord/bvngeecord.com/
+    cd bvngeecord.com
+else
+    cd bvngeecord.com
+    git pull
+fi
+
+# Install deps and packages
+npm install
+# Make the webroot and clear it
+mkdir -p /usr/share/nginx/html && rm -rf /usr/share/nginx/html/*
+# Build
+npm run build
+# Move all contents into webroot folder
+cp -r dist/* /usr/share/nginx/html
