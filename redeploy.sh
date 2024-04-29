84f8dc233e791c97d3bbe9d6c5b6b1f8b88a1318
Jack N <nystromjp@gmail.com>
2024-04-28 22:54:38 -0700

    dont use GH token for clones

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2164
 
 # Clone website repo if first time, else pull
 cd /root
 if ! [ -d "/root/bvngeecord.com/.git" ]; then
-    git clone https://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@github.com/BvngeeCord/bvngeecord.com/
+    git clone https://github.com/BvngeeCord/bvngeecord.com/
     cd bvngeecord.com
 else
     cd bvngeecord.com
